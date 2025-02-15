59d22fd84f80ca491d8568af8158281511d3a380
buyangdaffa <daffaeffendy2911@gmail.com>
2025-02-15 01:34:03 -0800

    adding pages for sub directory

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,34 +1,12 @@
 'use client';
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Header from '../components/Header';
 
 const About = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-sky-100 to-white">
-      {/* Header */}
-      <header className="px-[90px] pt-[21px] bg-[#BF0000] flex flex-col items-center">
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Dokdo&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
-        {/* Club Logo */}
-        <Link href="/" className="animate-character text-4xl text-[#F3F4F5] font-bold font-bebas mb-2">Garuda Nusantara</Link>
-
-        {/* Navbar */}
-        <nav className="w-full flex justify-center">
-          <div className="space-x-8 pb-3">
-            <a href="#About" className="text-xl font-semibold text-[#242024] hover:text-[#F3F4F5]">About</a>
-            <a href="#Team" className="text-xl font-semibold text-[#242024] hover:text-[#F3F4F5]">Team</a>
-            <a href="#Matches" className="text-xl font-semibold text-[#242024] hover:text-[#F3F4F5]">Matches</a>
-            <a href="#Media" className="text-xl font-semibold text-[#242024] hover:text-[#F3F4F5]">Media</a>
-            <a href="#News" className="text-xl font-semibold text-[#242024] hover:text-[#F3F4F5]">News</a>
-            <a href="#Fans" className="text-xl font-semibold text-[#242024] hover:text-[#F3F4F5]">Fans</a>
-            <a href="#Contact" className="text-xl font-semibold text-[#242024] hover:text-[#F3F4F5]">Contact</a>
-          </div>
-        </nav>
-      </header>
+      <Header />
 
       {/* Main Content */}
       <main className="flex-1">
